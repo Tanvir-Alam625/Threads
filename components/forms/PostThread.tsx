126079ac3ccf7cc1ccb97970ceb0c861ac63efb0
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-08-17 12:38:37 +0600

    chore: add design and validation on the create-thread page

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,13 +1,70 @@
+"use client";
+import * as z from "zod";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Button } from "@/components/ui/button"
+import { Textarea } from '@/components/ui/textarea';
+import { usePathname, useRouter } from "next/navigation"
+import { threadValidation } from "@/lib/validation/thread";
+
 
 type Props = {
     userId: string
 }
 
+
 const PostThread = ({ userId }: Props) => {
+    const pathname = usePathname();
+    const router = useRouter()
+    const form = useForm({
+        resolver: zodResolver(threadValidation),
+        defaultValues: {
+            thread: "",
+            accountId: userId,
+        },
+    });
+
+    const onSubmit = (values: z.infer<typeof threadValidation>) => {
+        console.log(values);
+    }
+
     return (
-        <div>
-            <h3 className="text-light-1">{userId}</h3>
-        </div>
+        <Form {...form}>
+            <form
+                className='mt-6 flex flex-col justify-start gap-6'
+                onSubmit={form.handleSubmit(onSubmit)}
+            >
+                {/* Name  */}
+                <FormField
+                    control={form.control}
+                    name='thread'
+                    render={({ field }) => (
+                        <FormItem className='flex flex-col items-start gap-2'>
+                            <FormLabel className='text-base-semibold text-light-2'>
+                                Content
+                            </FormLabel>
+                            <FormControl className='flex-1 text-base-semibold text-gray-200'>
+                                <Textarea
+                                    rows={10}
+                                    className='no-focus border-dark-4 bg-dark-3 text-light-1'
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" className="bg-primary-500">Post Thread</Button>
+            </form>
+        </Form>
     )
 }
 
