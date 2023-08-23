2a6bfa9f73c6edb0452284ab92a1e67fdaa8b018
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-08-23 20:37:03 +0600

    something changes

--- components/Skeleton/UserSkeleton.tsx
+++ components/Skeleton/UserSkeleton.tsx
@@ -8,11 +8,11 @@ const UserSkeleton = () => {
                     <div className='relative h-20 w-20 bg-gray-400 rounded-full animate-pulse' />
 
                     <div className='flex-1 flex flex-col gap-2'>
-                        <h4 className=' bg-gray-400 h-6 w-32 rounded-md animate-pulse' />
-                        <h4 className=' bg-gray-400 h-3 w-24 rounded-md animate-pulse' />
+                        <h4 className=' bg-gray-400 h-6 w-10 md:w-32 rounded-md animate-pulse' />
+                        <h4 className=' bg-gray-400 h-3 w-8 md:w-24 rounded-md animate-pulse' />
                     </div>
                 </div>
-                <div className='h-8 w-24 mt-4 bg-gray-400 rounded-md animate-pulse' />
+                <div className='h-8  w-24 mt-4 bg-gray-400 rounded-md animate-pulse' />
             </div>
             <div className='h-32 w-full  bg-gray-400 rounded-md animate-pulse' />
             <div className='h-10 w-full my-6 bg-gray-400 rounded-md animate-pulse' />
