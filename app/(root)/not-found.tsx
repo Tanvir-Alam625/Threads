9df0d8f58018f561a06eaff0da2a3f930f7a4e1f
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2024-01-13 00:46:05 +0600

    chore: add not found page

---
+++ app/(root)/not-found.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Fira_Code } from "next/font/google"
+
+const firaCode = Fira_Code({ subsets: ['latin'] });
+export default function NotFound() {
+    return (
+        <div className="min-h-svh flex flex-col gap-3 justify-center items-center">
+            <h2 className={`${firaCode.className} text-xl text-rose-500 font-semibold my-2`}>404</h2>
+            <p className="text-center text-small-regular text-gray-1 font-medium">Could not find requested resource</p>
+            <Link href="/">
+                <Button>Go Back Home</Button>
+            </Link>
+        </div>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,8 @@ module.exports = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
+    "./ui/**/*.{ts,tsx}",
+    "./app/*.{tsx,ts}"
   ],
   theme: {
     container: {
